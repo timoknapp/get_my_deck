aeebe752b54866ddd02789dcdd640d18e9e661e7
timoknapp <mail@timoknapp.com>
2025-01-11 11:14:51 +0100

    feat: add Signal support

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Steam Deck Refurbished Stock Checker
 
-A simple scraper which checks the Steam Deck Refurb page for stock. This script sends email notifications when the Steam Deck is in stock.
+A simple scraper which checks the Steam Deck Refurb page for stock. This script sends notifications when the Steam Deck is in stock.
 
 ## Requirements
 
@@ -9,14 +9,18 @@ A simple scraper which checks the Steam Deck Refurb page for stock. This script 
   ```sh
   pip install selenium webdriver-manager
   ```
-- Email account for sending notifications
+- Email account or [Signal account](https://github.com/bbernhard/signal-cli-rest-api) for sending notifications
 
 ## Getting Started
 
 ### Command Line
 
 ```sh
-python get_my_deck.py --email <your_email> --password <your_password> --send_to_email <recipient_email> --smtp_host <smtp_host> [--test_email] [--refresh_time <seconds>]
+python get_my_deck.py --email <your_email> --password <your_password> --send_to_email <recipient_email> --smtp_host <smtp_host> [--test_notification] [--refresh_time <seconds>]
+```
+or
+```sh
+python get_my_deck.py --signal_api_url <signal_api_url> --signal_number <signal_number> --send_to_number <recipient_number> [--test_notification] [--refresh_time <seconds>]
 ```
 
 ### Docker
@@ -34,7 +38,20 @@ python get_my_deck.py --email <your_email> --password <your_password> --send_to_
        -e PASSWORD=<your_password> \
        -e SEND_TO_EMAIL=<recipient_email> \
        -e SMTP_HOST=<smtp_host> \
-       -e TEST_EMAIL=<true|false> \
+       -e TEST_NOTIFICATION=<true|false> \
+       -e REFRESH_TIME=<seconds> \
+       -e DEVICES_TO_MONITOR=<devices> \
+       get_my_deck
+   ```
+or
+   ```sh
+   docker run \
+       -d \
+       --name get_my_deck \
+       -e SIGNAL_API_URL=<signal_api_url> \
+       -e SIGNAL_NUMBER=<signal_number> \
+       -e SEND_TO_NUMBER=<recipient_number> \
+       -e TEST_NOTIFICATION=<true|false> \
        -e REFRESH_TIME=<seconds> \
        -e DEVICES_TO_MONITOR=<devices> \
        get_my_deck
@@ -46,14 +63,17 @@ You can pass the following parameters to the script via the command line or envi
 
 ### Arguments
 
-| Parameter       | Description                                      | Required | Default |
-|-----------------|--------------------------------------------------|----------|---------|
-| `--email`       | Email address to send notifications from         | Yes      | N/A     |
-| `--password`    | Password for the email account                   | Yes      | N/A     |
-| `--send_to_email` | Email address to send notifications to         | Yes      | N/A     |
-| `--smtp_host`   | SMTP host for sending email                      | Yes      | N/A     |
-| `--test_email`  | Send a test email and exit                       | No       | False   |
-| `--refresh_time`| Time in seconds between page refreshes           | No       | 3600    |
+| Parameter             | Description                                      | Required | Default |
+|-----------------------|--------------------------------------------------|----------|---------|
+| `--email`             | Email address to send notifications from         | Yes (if using email) | N/A     |
+| `--password`          | Password for the email account                   | Yes (if using email) | N/A     |
+| `--send_to_email`     | Email address to send notifications to           | Yes (if using email) | N/A     |
+| `--smtp_host`         | SMTP host for sending email                      | Yes (if using email) | N/A     |
+| `--signal_api_url`    | Signal API URL for sending notifications         | Yes (if using Signal) | N/A     |
+| `--signal_number`     | Signal number to send notifications from         | Yes (if using Signal) | N/A     |
+| `--send_to_number`    | Signal number to send notifications to           | Yes (if using Signal) | N/A     |
+| `--test_notification` | Send a test notification and exit                | No       | False   |
+| `--refresh_time`      | Time in seconds between page refreshes           | No       | 3600    |
 | `--devices_to_monitor`| List of devices to monitor, separated by space. e.g. "Steam Deck 512 GB OLED" "Steam Deck 1TB OLED". Use the names from this [page](https://store.steampowered.com/sale/steamdeckrefurbished/) | No       | All devices |
 
 ### Environment Variables
@@ -64,9 +84,16 @@ You can also set the parameters via environment variables:
 - `PASSWORD`
 - `SEND_TO_EMAIL`
 - `SMTP_HOST`
-- `TEST_EMAIL` (set to `true` to send a test email and exit)
+- `SIGNAL_API_URL`
+- `SIGNAL_NUMBER`
+- `SEND_TO_NUMBER`
+- `TEST_NOTIFICATION` (set to `true` to send a test notification and exit)
 - `REFRESH_TIME` (time in seconds between page refreshes, default is 3600)
 - `DEVICES_TO_MONITOR` (comma-separated list of devices to monitor, e.g. "Steam Deck 512 GB OLED,Steam Deck 1TB OLED". Use the names from this [page](https://store.steampowered.com/sale/steamdeckrefurbished/), default is all devices)
+
+## Note
+
+Only one type of notification method (email or Signal) is supported at a time. If both types of parameters are provided, the script will show an error message and quit.
 
 ## Credits
 
--- get_my_deck.py
+++ get_my_deck.py
@@ -2,6 +2,7 @@ import smtplib
 import argparse
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import requests
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -47,7 +48,22 @@ def send_email(deckname, deckprice, email, password, send_to_email, smtp_host):
     server.sendmail(email, send_to_email, text)
     server.quit()
 
-def check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, devices_to_monitor):
+def send_signal_message(deck, deckprice, signal_api_url, signal_number, send_to_number):
+    log(f"Sending Signal message for {deck}")
+    message = f"{deck} in Stock for {deckprice} https://store.steampowered.com/sale/steamdeckrefurbished"
+    payload = {
+        "message": message,
+        "number": signal_number,
+        "recipients": [send_to_number]
+    }
+    response = requests.post(f"{signal_api_url}/v2/send", json=payload)
+    log(f"Signal API response: {response.status_code} - {response.text}")
+    if response.status_code in [200, 201]:
+        log("Signal message sent successfully.")
+    else:
+        log(f"Failed to send Signal message: {response.text}")
+
+def check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, devices_to_monitor):
     decktitle = decktitle.strip()
     deckname = decktitle.split(" - ")[0]
     if devices_to_monitor and devices_to_monitor != [''] and deckname not in devices_to_monitor:
@@ -56,20 +72,23 @@ def check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to
     if "add" in deckstatus.lower():
         if deckname not in sent_notifications_for_devices:
             log(f"{deckname} - In Stock for {deckprice}")
-            send_email(deckname, deckprice, email, password, send_to_email, smtp_host)
+            if email and password and send_to_email and smtp_host:
+                send_email(deckname, deckprice, email, password, send_to_email, smtp_host)
+            elif signal_api_url and signal_number and send_to_number:
+                send_signal_message(deckname, deckprice, signal_api_url, signal_number, send_to_number)
             sent_notifications_for_devices.add(deckname)
         else:
-            log(f"{deckname} - Still in Stock for {deckprice}, not sending another email")
+            log(f"{deckname} - Still in Stock for {deckprice}, not sending another notification")
         status = 1
     else:
         log(f"{deckname} - Out of Stock")
         if deckname in sent_notifications_for_devices:
-            log(f"{deckname} - Out of Stock again, will send email if it comes back in stock")
+            log(f"{deckname} - Out of Stock again, will send notification if it comes back in stock")
             sent_notifications_for_devices.remove(deckname)
         status = 0
     return status
 
-def runner(driver, email, password, send_to_email, smtp_host, devices_to_monitor):
+def runner(driver, email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, devices_to_monitor):
     all_btn = driver.find_elements(By.XPATH, "//*[@id='SaleSection_33131']")
     if not all_btn:
         log("No elements found with the given XPath.")
@@ -81,21 +100,24 @@ def runner(driver, email, password, send_to_email, smtp_host, devices_to_monitor
         deckstatus = decks[i+1]
         deckprice = decks[i+2]
         if "512 GB OLED" in decktitle:
-            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, devices_to_monitor)
+            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, devices_to_monitor)
         elif "1TB OLED" in decktitle:
-            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, devices_to_monitor)
+            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, devices_to_monitor)
         elif "64 GB LCD" in decktitle:
-            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, devices_to_monitor)
+            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, devices_to_monitor)
         elif "256 GB LCD" in decktitle:
-            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, devices_to_monitor)
+            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, devices_to_monitor)
         elif "512 GB LCD" in decktitle:
-            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, devices_to_monitor)
+            check_deck_status(decktitle, deckstatus, deckprice, email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, devices_to_monitor)
     return
 
-def get_my_deck(email, password, send_to_email, smtp_host, test_email, refresh_time, devices_to_monitor):
-    if test_email:
-        send_email("Test Deck", "0", email, password, send_to_email, smtp_host)
-        log("Test email sent. Exiting...")
+def get_my_deck(email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, test_notification, refresh_time, devices_to_monitor):
+    if test_notification:
+        if email and password and send_to_email and smtp_host:
+            send_email("Test Deck", "0", email, password, send_to_email, smtp_host)
+        elif signal_api_url and signal_number and send_to_number:
+            send_signal_message("Test Deck", "0", signal_api_url, signal_number, send_to_number)
+        log("Test notification sent. Exiting...")
         return
 
     start_time = 10
@@ -104,7 +126,7 @@ def get_my_deck(email, password, send_to_email, smtp_host, test_email, refresh_t
     log("Started Scraper")
     while True:
         try:
-            runner(driver, email, password, send_to_email, smtp_host, devices_to_monitor)
+            runner(driver, email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, devices_to_monitor)
             refresh(driver)
             log(f"Reloading page in {refresh_time} seconds...")
             time.sleep(refresh_time)  # Refresh the page once per minute
@@ -119,7 +141,10 @@ if __name__ == "__main__":
     parser.add_argument('--password', help='Password for the email account')
     parser.add_argument('--send_to_email', help='Email address to send notifications to')
     parser.add_argument('--smtp_host', help='SMTP host for sending email')
-    parser.add_argument('--test_email', action='store_true', help='Send a test email and exit')
+    parser.add_argument('--signal_api_url', help='Signal API URL for sending notifications')
+    parser.add_argument('--signal_number', help='Signal number to send notifications from')
+    parser.add_argument('--send_to_number', help='Signal number to send notifications to')
+    parser.add_argument('--test_notification', action='store_true', help='Send a test notification and exit')
     parser.add_argument('--refresh_time', type=int, default=3600, help='Time in seconds between page refreshes')
     parser.add_argument('--devices_to_monitor', nargs='+', help='List of devices to monitor, separated by space. e.g. "Steam Deck 512 GB OLED" "Steam Deck 1TB OLED"')
     args = parser.parse_args()
@@ -129,11 +154,16 @@ if __name__ == "__main__":
     password = args.password or os.getenv('PASSWORD')
     send_to_email = args.send_to_email or os.getenv('SEND_TO_EMAIL')
     smtp_host = args.smtp_host or os.getenv('SMTP_HOST')
-    test_email = args.test_email or os.getenv('TEST_EMAIL', 'false').lower() == 'true'
+    signal_api_url = args.signal_api_url or os.getenv('SIGNAL_API_URL')
+    signal_number = args.signal_number or os.getenv('SIGNAL_NUMBER')
+    send_to_number = args.send_to_number or os.getenv('SEND_TO_NUMBER')
+    test_notification = args.test_notification or os.getenv('TEST_NOTIFICATION', 'false').lower() == 'true'
     refresh_time = args.refresh_time or int(os.getenv('REFRESH_TIME', 3600))
     devices_to_monitor = args.devices_to_monitor or os.getenv('DEVICES_TO_MONITOR', '').split(',')
 
-    if not email or not password or not send_to_email or not smtp_host:
-        parser.error('Email, password, send_to_email, and smtp_host must be provided either as arguments or environment variables.')
+    if (email and password and send_to_email and smtp_host) and (signal_api_url and signal_number and send_to_number):
+        parser.error('Both email and Signal parameters provided. Please provide only one type of notification method.')
+    elif not (email and password and send_to_email and smtp_host) and not (signal_api_url and signal_number and send_to_number):
+        parser.error('Neither email nor Signal parameters provided. Please provide one type of notification method.')
 
-    get_my_deck(email, password, send_to_email, smtp_host, test_email, refresh_time, devices_to_monitor)
+    get_my_deck(email, password, send_to_email, smtp_host, signal_api_url, signal_number, send_to_number, test_notification, refresh_time, devices_to_monitor)
